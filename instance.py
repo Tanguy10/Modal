9223a857fdb5c1ca467155604435086a17b8c367
victorgalmiche <157879404+victorgalmiche@users.noreply.github.com>
2024-04-12 09:36:19 +0200

    Update instance.py
    Création d'instance modifiée

--- instance.py
+++ instance.py
@@ -1,15 +1,19 @@
 import numpy as np
-from scipy.stats import randint
+from scipy.stats import randint, expon, bernoulli
 
 
-def offline(n, l):
-    """Crée une instance sous forme de deux listes, une pour les entrées et l'autre pour les sorties
-    n est le nombre de requêtes d'entrées et de sortie
-    l est le nombre d'étages"""
-    entrées, sorties = []
-    for i in range(n) :
-        etage = randint(0, l+1)
-        entrées.append(etage)
-        etage = randint(0, l+1)
-        sorties.append(etage)
-    return entrées, sorties
+def offline(l=5, T=1000):
+    """Crée une instance sous forme d'une liste de requêtes contenant un triplet 
+    (temps d'arrivée, étage, entrée/sortie) jusqu'au temps T"""
+    requests = []
+    t = 0 #Le compteur temps
+    while t < T:
+        d = expon.rvs() #Pour l'instant lambda = 1
+        t += d #On avance dans le temps
+        etage = randint.rvs(0, l+1) #L'étage de la requête
+        b = bernoulli.rvs(0.5) #Permet de choisir entre entrée ou sortie
+        if b==0 :
+            requests.append((t, etage, 'e'))
+        else :
+            requests.append((t, etage, 's'))
+    return requests
